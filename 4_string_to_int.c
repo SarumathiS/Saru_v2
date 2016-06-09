0a9d562c83bba2ae77bd96edb0ab5e9ac74f7887
SarumathiS <sarusm3796@gmail.com>
2016-06-09 16:31:26 +0530

    Create 4_string_to_int.c

---
+++ 4_string_to_int.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+   int num;
+   char s[3];
+
+  
+   scanf("%s",s);
+
+   num = atoi(s);
+   printf("\nresult is : %d", num);
+
+   return (0);
+}
